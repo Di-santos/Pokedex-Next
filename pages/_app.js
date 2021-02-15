7c745d44155125ef4054c918d0cd84fc95ad077a
Di-santos <dio.silveira07@gmail.com>
2021-02-15 18:20:30 -0300

    v 0.1.0
    - commit inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,49 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+/*
+O _app é uma ferramenta do styled components que permite um maior controle de escopo
+do css da aplicação, definindo temas reutilizáveis (quase um super root do css padrão)
 
+Além disso, você pode importar temas e variáveis prontas a partir de um JSON externo
+e incluir de maneira simples neste arquivo, como com o db.json (não é exclusivo do
+next ou do SC, mas eles favorecem esta prática)
+
+*/
+
+
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
+
+// Estilo global
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+  }
+
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+// Temas estruturados
+const theme = db.theme
 
+// Instanciação (troca tema com global)
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />   
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+/**
+Funções no React representam os componentes
+Com styled components, pode-se definir e estilizar tais componentes de maneira simplificada (pré-configurada)
+**/ 
+
 import styled from 'styled-components'
 
 const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+    font-size: 50px;
+    color: ${({ theme }) => theme.colors.primary};
 `
+// function Welcome(props){
+//     return (
+//     <p> {props.children} </p>
+//     )
+// }
 
 export default function Home() {
-  return <Title>My page</Title>
+    return (
+        <Title>
+            Dioguin
+        </Title>
+    )
 }
