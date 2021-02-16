78dcddde60c6e03fd1373f7dba7e2c0fb846a450
Di-santos <dio.silveira07@gmail.com>
2021-02-16 12:14:41 -0300

    v 0.1.1
    - Correções no global
    - Início da construção dos widgets

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@
 O _app é uma ferramenta do styled components que permite um maior controle de escopo
 do css da aplicação, definindo temas reutilizáveis (quase um super root do css padrão)
 
+Desta forma, você pode predefinir, centralizar e conectar todo o css do seu projeto, 
+desde cores, border-radius, fontes e até mesmo conteúdo bruto em si
+
 Além disso, você pode importar temas e variáveis prontas a partir de um JSON externo
 e incluir de maneira simples neste arquivo, como com o db.json (não é exclusivo do
 next ou do SC, mas eles favorecem esta prática)
 
 */
-
 
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from '../db.json'
@@ -18,6 +20,11 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
   
+  section{
+    min-height: 100vh;
+    min-width: 100vw;
+  }
+
   body {
     margin: 0;
     padding: 0;
@@ -31,14 +38,13 @@ const GlobalStyle = createGlobalStyle`
     flex: 1;
     display: flex;
     flex-direction: column;
-
   }
 `
 
-// Temas estruturados
+// Tema da Aplicação
 const theme = db.theme
 
-// Instanciação (troca tema com global)
+// Instanciação (troca tema com global - global dentro de themeprovider)
 export default function App({ Component, pageProps }) {
   return (
     <>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,68 @@
 /**
 Funções no React representam os componentes
 Com styled components, pode-se definir e estilizar tais componentes de maneira simplificada (pré-configurada)
+
+Componente Padrão
+function Welcome(props){
+    return (
+    <p> {props.children} </p>
+    )
+}
+
 **/ 
 
 import styled from 'styled-components'
+import db from '../db.json'
 
+// Styled Components
+const Fundo = styled.section`
+    background-image: url(${db.bg});
+    flex: 1;
+    background-size: cover;
+    background-position: center;
+`
 const Title = styled.h1`
-    font-size: 50px;
+    font-size: 100px;
     color: ${({ theme }) => theme.colors.primary};
 `
-// function Welcome(props){
-//     return (
-//     <p> {props.children} </p>
-//     )
-// }
+const Quiz = styled.div`
+    width: 20%;
+    padding-top: 50px;
+    margin: auto 10%;
+    max-width: 300px;
+`
+const Widget = styled.div`
+    margin-top: 24px;
+    margin-bottom: 24px;
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+    border-radius: ${({ theme }) => theme.borderRadius};
+    background-color: #1C1814;
+    overflow: hidden;
+
+    h1,h2,h3{
+        font-size: 16px;
+        font-weight: 700;
+        line-height: 1;
+    }
+
+    p{
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 1;
+    }
+`
 
 export default function Home() {
-    return (
-        <Title>
-            Dioguin
-        </Title>
-    )
-}
+        return (
+            <Fundo>
+                <Quiz>
+                    <Widget>
+                        <Title>
+                            Evangelion
+                        </Title>
+                    </Widget>
+                </Quiz>
+            </Fundo>
+        )
+    }
+
