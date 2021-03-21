f0ca39fb4e11a2c99935393519873679b8b70a95
Di-santos <dio.silveira07@gmail.com>
2021-03-21 15:18:20 -0300

    v0.9.5
    - Auxílio externo API

---
+++ pages/api/terminal/index.js
@@ -0,0 +1,11 @@
+import info from '../../../src/db/data.json'
+
+export default function handler(req, res) {
+    try{
+        res.status(200).json({info})
+
+    }
+    catch(error){
+        res.status(500).json({ message: error.message})
+    }
+  }
